e4bef86655e60f1a6baae72d9b9a40348505bbf6
taciovinicius <taciovinicius@7ee5c129-e14f-0410-8cc6-57a669be0a67>
2008-07-22 01:32:47 +0000

    Regressao ok, só falta melhorar pra ficar retinha a linha

--- spc-controlchart/src/controlcharts/RegressionChartLimits.java
+++ spc-controlchart/src/controlcharts/RegressionChartLimits.java
@@ -5,63 +5,101 @@ import java.util.ArrayList;
 import org.apache.commons.math.MathException;
 import org.apache.commons.math.stat.regression.SimpleRegression;
 
+import data.DataSetException;
 import data.DataSetItem;
+import data.DataSetIterator;
 
 public class RegressionChartLimits implements GenericChartLimits
 {
 	private SimpleRegression regressao;
+	private double erro_padrao;
 	
-	public RegressionChartLimits(SimpleRegression regressao)
+	public RegressionChartLimits(SimpleRegression regressao, DataSetIterator data_set) throws DataSetException
 	{
 		this.regressao = regressao;
-		System.out.println(this.regressao.getInterceptStdErr());
-		System.out.println(this.regressao.getMeanSquareError());
-		System.out.println(this.regressao.getSlopeStdErr());
-		System.out.println(this.regressao.getSumSquaredErrors());
-		System.out.println(this.regressao.getN());
-		System.out.println(this.regressao.getR());
-		System.out.println(this.regressao.getRegressionSumSquares());
-		System.out.println(this.regressao.getRSquare());
-		try {
-			System.out.println(this.regressao.getSignificance());
-			System.out.println(this.regressao.getSlopeConfidenceInterval());
-			System.out.println(this.regressao.getInterceptStdErr());
-		} catch (MathException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		System.out.println(this.regressao.getSumSquaredErrors());
+		this.erro_padrao = this.calcular_erro_padrao(data_set);
+		System.out.println("Erro padrao:");
+		System.out.println(this.erro_padrao);
 	}
 
 	public void addData(ArrayList<DataSetItem> new_data) 
 	{
 		//this.data.add(new_data);
 	}
+	
+	private double calcular_erro_padrao(DataSetIterator data_set) throws DataSetException
+	{
+		//int cont = 0;
+
+		double y1 = 0;
+		double y2 = 0;
+		double desvio = 0;
+		double diferenca = 0;
+		double n = 0;
+
+		Integer tamanho_amostra = null;
+		while(!data_set.isEmpty())
+		{
+			//cont++;
+			ArrayList<DataSetItem> item = data_set.next();
+			int tamanho = item.size();
+			if(tamanho_amostra==null)
+			{
+				double x = 0;
+				double y = 0;
+				for(int cont2=0;(cont2<2 && cont2<tamanho);cont2++)
+				{
+					DataSetItem sample_part = item.get(cont2);
+					if(cont2==0)
+					{
+						x = sample_part.getY();
+					}
+					else
+					{
+						y = sample_part.getY();
+					}
+				}//fim-for
+				y2 = this.regressao.predict(x);
+				y1 = y;
+				//somar os desvios
+				diferenca = (y1 - y2);
+				double dif_qudra = Math.pow(diferenca, 2);						
+				desvio = desvio + (dif_qudra);
+				n++;
+				//this.regressao.addData(x, y);
+			}//fim-if
+		}//fim-while
+		double denominador = n-2;
+		double erro = Math.sqrt(desvio/denominador);
+		
+		return erro;
+	}
 
 	public Double calculateCentralLine(Double x) 
 	{
 		return this.regressao.predict(x);
 	}
+	
+	
 
-	public Double calculateLowerAdvertenceLimit(Double x) {
-		// TODO Auto-generated method stub
-		return 1.0;
+	public Double calculateLowerAdvertenceLimit(Double x) 
+	{
+		return this.regressao.predict(x)-(1.5*this.erro_padrao);
 	}
 
-	public Double calculateLowerControlLimit(Double x) {
-		// TODO Auto-generated method stub
-		return 1.0;
+	public Double calculateLowerControlLimit(Double x) 
+	{
+		return this.regressao.predict(x)-(3.0*this.erro_padrao);
 	}
 
-	public Double calculateUpperAdvertenceLimit(Double x) {
-		// TODO Auto-generated method stub
-		return 1.0;
+	public Double calculateUpperAdvertenceLimit(Double x) 
+	{
+		return this.regressao.predict(x)+(1.5*this.erro_padrao);
 	}
 
-	public Double calculateUpperControlLimit(Double x) {
-		// TODO Auto-generated method stub
-		return 1.0;
+	public Double calculateUpperControlLimit(Double x) 
+	{
+		return this.regressao.predict(x)+(3.0*this.erro_padrao);
 	}
 	
 	/**
