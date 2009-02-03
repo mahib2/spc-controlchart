2b30bc60a51c560d5ec1d1e14550db97be0bc4ad
taciovinicius <taciovinicius@7ee5c129-e14f-0410-8cc6-57a669be0a67>
2009-02-03 21:00:56 +0000

    andei ajeitando um, e fiz o outro.

--- trunk/spc-controlchart/src/statistic/CusumStatistic.java
+++ trunk/spc-controlchart/src/statistic/CusumStatistic.java
@@ -10,6 +10,7 @@ import org.apache.commons.math.stat.descriptive.rank.Median;
 
 import types.DataConverter;
 import types.DoubleDataConverter;
+import types.TypeUtilities;
 
 import data.DataSetCsvIterator;
 import data.DataSetException;
@@ -19,51 +20,78 @@ import data.DataSetIterate;
 public class CusumStatistic implements GenericStatistic{
 
 	private boolean comportamento;
-	private Double maximo = 0.0;
+	private Double k;
 	private static ArrayList<ArrayList<DataSetItem>> data;
 	private static Integer sample_size;
+	private Double ci_anterior = 0.0;
+	private double standard_deviation;
+	private double average;
 	
 	
-	public CusumStatistic(boolean tipo_comportamento)
+	public CusumStatistic(boolean tipo_comportamento, double k, double standard_deviation, double average)
 	{
 		this.comportamento = tipo_comportamento;
+		this.standard_deviation = standard_deviation;
+		this.average = average;
+		//TODO ver se nao tem que char o init em algum momento
 	}
 	
-	private Double ci_anterior = 0.0;
+	private double calculate_k(double k, double standard_deviation, double numero_amostras)
+	{
+		return (k*standard_deviation)/Math.sqrt(numero_amostras);						
+	}
 	
 	public Double generateStatistic(ArrayList<DataSetItem> sample) 
 	{
+		//TODO acho que falta so o N+/- aqui.
+		if(this.k==null)
+		{
+			this.k = this.calculate_k(this.k, this.standard_deviation, CusumStatistic.sample_size);
+		}
+		
+		if(sample==null)
+		{
+			return null;
+		}
+		
+		int tamanho = sample.size();
+		Double media_amostra = null;
+		
+		/*Acha a media 1º*/
+		for(int cont=0;cont<tamanho;cont++)
+		{
+			DataSetItem sample_part = sample.get(cont);
+			media_amostra = TypeUtilities.sumNumbers(sample_part.getY(),media_amostra);
+		}
+		media_amostra = TypeUtilities.divideNumbers(media_amostra,new Double(tamanho));
+		
+		/*Agora sim o Ci*/
 		Double retorno = 0.0;
-		Double xi =0.0;
 		if(sample.size()>0)
 		{
+			double ci;
 			if(this.comportamento)
 			{
-				xi = sample.get(0).getY()+this.ci_anterior;
-				//TODO COLOCAR O MI+k na magnitude				
-				Double magnitude = 0.0;
-				magnitude = this.getPositiveMagnitude(sample);
-				retorno = xi-magnitude;				
+				ci = media_amostra - (this.average+this.k);
 			}
 			else
 			{
-				xi = sample.get(0).getY()+this.ci_anterior;
-				//TODO COLOCAR O MI-k na magnitude
-				Double magnitude = 0.0;
-				magnitude = this.getNegativeMagnitude(sample);
-				retorno = magnitude-xi;	
-			}			
+				ci = (this.average-this.k) - media_amostra;
+			}
+			retorno = ci+ this.ci_anterior;
 		}
 		
-		if(retorno>this.maximo)
+		if(retorno<0.0)
 		{
-			this.maximo = retorno;
+			retorno = 0.0;
 		}
 		
-		this.ci_anterior = this.maximo;
-		return this.maximo;		
+		this.ci_anterior = retorno.doubleValue();
+		
+		return retorno;		
 	}
 	
+	/*
 	public Double getPositiveMagnitude(ArrayList<DataSetItem> sample)
 	{
 		Double desvio = this.getStandardDeviation(this);
@@ -117,6 +145,7 @@ public class CusumStatistic implements GenericStatistic{
 		System.out.println("Desvio padrão: "+retorno);
 		return retorno;
 	}
+	*/
 
 	public static void addData(ArrayList<DataSetItem> new_data) 
 	{
